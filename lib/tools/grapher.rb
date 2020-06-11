efd5810ae1b06406f381d96400a71c8b0de07864
ZIming Mao <ziming.mao@yale.edu>
2020-06-11 16:52:25 -0400

    minor changes

--- lib/tools/grapher.rb
+++ lib/tools/grapher.rb
@@ -5,7 +5,5 @@ class Grapher
         graph1 = Scruffy::Graph.new
         graph1.add(:line, 'US', [100, -20, 30, 60])
         graph1.render
-        # output.each do |entry|
-        #     entry[1]
     end
 end
